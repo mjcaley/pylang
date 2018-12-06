024d177acccba8f27459dde85f84ea849aef0995
Michael Caley <mjcaley@darkarctic.com>
2018-12-06 15:27:58 -0500

    Added LLVM blocks

--- pylang/parser.py
+++ pylang/parser.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
 from lark import Lark
-
+import llvmlite.binding as llvm
+import llvmlite.ir as ir
 
 PYLANG_GRAMMAR = '''
-    start: NUMBER
+    start: statement+
+    
+    statement: sum_expr EOL
+    
+    sum_expr: NUMBER ADD_OP NUMBER
+    
+    EOL: ";"
     
     ADD_OP: "+"
     SUB_OP: "-"
@@ -20,4 +27,15 @@ PYLANG_GRAMMAR = '''
 
 parser = Lark(PYLANG_GRAMMAR)
 
-print(parser.parse("42"))
+print(parser.parse("42 + 24;"))
+
+
+llvm.initialize()
+llvm.initialize_native_target()
+llvm.initialize_native_asmprinter()
+
+module = ir.Module()
+sum_func = ir.Function(module, ir.FunctionType(ir.IntType(32), [ir.IntType(32), ir.IntType(32)]), 'sum')
+
+block = sum_func.append_basic_block()
+block.
