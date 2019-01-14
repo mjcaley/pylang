ca905907f6921d13aac6d45fa24d91e9613d0216
Michael Caley <mjcaley@darkarctic.com>
2019-01-14 14:54:31 -0500

    Added assignment expressions

--- pylang/parser.py
+++ pylang/parser.py
@@ -5,11 +5,13 @@ from lark import Lark
 
 GRAMMAR = '''
     start: statement+
-    
+
     statement: _expr ";"
-    
+
     _expr:   binary_expr
-    ?binary_expr:    _sum_expr
+    ?binary_expr:    _assign_expr
+    _assign_expr:   _sum_expr
+                    | _sum_expr ASSIGN_OP _expr
     _sum_expr:  _prod_expr
                 | _prod_expr ADD_OP _expr
                 | _prod_expr SUB_OP _expr
@@ -22,11 +24,14 @@ GRAMMAR = '''
     integer: INT
     float: DECIMAL
     bool: [TRUE | FALSE]
+    identifier: CNAME
     ?atom:  integer
             | float
             | bool
+            | identifier
             | _PAREN_L _expr _PAREN_R
-    
+
+    ASSIGN_OP: "="
     ADD_OP: "+"
     SUB_OP: "-"
     MUL_OP: "*"
@@ -35,15 +40,16 @@ GRAMMAR = '''
     _PAREN_R: ")"
     NEGATIVE_OP: "-"
     NOT_OP: "!"
-    
+
     TRUE: "true"
     FALSE: "false"
-    
+
     NULL: "null"
-    
+
     %import common.WORD
     %import common.INT
     %import common.DECIMAL
+    %import common.CNAME
     %ignore " "
 '''
 
