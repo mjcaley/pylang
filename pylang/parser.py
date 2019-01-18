6c578ef6ab6d6d6c3b7da23e4a0bf547230da8a3
Michael Caley <mjcaley@darkarctic.com>
2019-01-18 09:40:46 -0500

    Moved grammar to its own file

--- pylang/parser.py
+++ pylang/parser.py
@@ -1,57 +1,14 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
+
 from lark import Lark
 
 
-GRAMMAR = '''
-    start: statement+
-
-    statement: _expr ";"
-
-    _expr:   binary_expr
-    ?binary_expr:    _assign_expr
-    _assign_expr:   _sum_expr
-                    | _sum_expr ASSIGN_OP _expr
-    _sum_expr:  _prod_expr
-                | _prod_expr ADD_OP _expr
-                | _prod_expr SUB_OP _expr
-    _prod_expr: unary_expr
-                | unary_expr MUL_OP _expr
-                | unary_expr DIV_OP _expr
-    ?unary_expr:    NEGATIVE_OP unary_expr
-                    | NOT_OP unary_expr
-                    | atom
-    integer: INT
-    float: DECIMAL
-    bool: [TRUE | FALSE]
-    identifier: CNAME
-    ?atom:  integer
-            | float
-            | bool
-            | identifier
-            | _PAREN_L _expr _PAREN_R
-
-    ASSIGN_OP: "="
-    ADD_OP: "+"
-    SUB_OP: "-"
-    MUL_OP: "*"
-    DIV_OP: "/"
-    _PAREN_L: "("
-    _PAREN_R: ")"
-    NEGATIVE_OP: "-"
-    NOT_OP: "!"
-
-    TRUE: "true"
-    FALSE: "false"
-
-    NULL: "null"
-
-    %import common.WORD
-    %import common.INT
-    %import common.DECIMAL
-    %import common.CNAME
-    %ignore " "
-'''
+def grammar():
+    with open(Path(__file__).parent / 'pylang.lark') as g:
+        return g.read()
 
 
-parser = Lark(GRAMMAR, parser='lalr')
+def parser():
+    return Lark(grammar(), parser='lalr')
--- test/conftest.py
+++ test/conftest.py
@@ -6,12 +6,12 @@ import pytest
 @pytest.fixture
 def parser():
     from lark import Lark
-    from pylang.parser import GRAMMAR
+    from pylang.parser import grammar
 
     def inner(start_rule=None):
         if start_rule:
-            return Lark(GRAMMAR, parser='lalr', start=start_rule)
+            return Lark(grammar(), parser='lalr', start=start_rule)
         else:
-            return Lark(GRAMMAR, parser='lalr')
+            return Lark(grammar(), parser='lalr')
 
     return inner
