95678729b365e6202bace5ff025eae0468b481c3
Michael Caley <mjcaley@darkarctic.com>
2019-03-11 11:27:40 -0400

    Added constants for whitespace

--- pylang/lexer.py
+++ pylang/lexer.py
@@ -64,6 +64,19 @@ prime lexer with newline as next, start and end position is -1 index
 emit token, emits newline
 """
 
+SKIP = [
+        # ASCII whitespace characters
+        '\t', '\n', '\v', '\f', '\r', ' ',
+
+        # Unicode whitespace
+        '\u0085', '\u00a0', '\u1680', '\u2000', '\u2001', '\u2002', '\u2003', '\u2004', '\u2005', '\u2006', '\u2007',
+        '\u2008', '\u2009', '\u200a', '\u2028', '\u2029', '\u202f', '\u205f', '\u3000',
+
+        # Other whitespace characters
+        '\u180e', '\u200b', '\u200c', '\u200d', '\u2060', '\ufeff']
+NEWLINE = ['\n', '\r']
+INDENT = ['\t', ' ']
+
 
 class LexerException(Exception):
     pass
