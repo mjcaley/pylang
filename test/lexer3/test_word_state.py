ffe908743777e99db2b43a547084783cbc798852
Michael Caley <mjcaley@darkarctic.com>
2019-08-21 07:29:11 -0400

    Added test for Word to make sure it stops at reserved characters

--- test/lexer3/test_word_state.py
+++ test/lexer3/test_word_state.py
@@ -49,3 +49,24 @@ def test_returns_keyword(context_at_current, test_input, expected, mocker):
     assert result[1].position.line == 1
     assert result[1].position.column == 1
     assert w.context.advance.call_count == len(test_input)
+
+
+@pytest.mark.parametrize('test_input', [
+    '\v', '\f', ' ', '\u0085', '\u00a0', '\u1680', '\u2000', '\u2001', '\u2002', '\u2003', '\u2004', '\u2005', '\u2006',
+    '\u2007', '\u2008', '\u2009', '\u200a', '\u2028', '\u2029', '\u202f', '\u205f', '\u3000', '\u180e', '\u200b',
+    '\u200c', '\u200d', '\u2060', '\ufeff', '\n', '\r', '\t', ' ',
+
+    '+', '-', '/', '*', '%', '^',
+
+    '(', ')', '[', ']', '{', '}',
+
+    '!', '=', '<', '>', '.', ':', ','
+])
+def test_stops_at_reserved_characters(context_at_current, mocker, test_input):
+    expected = 'abc123'
+    w = Word(context_at_current(expected + test_input))
+    mocker.spy(w.context, 'advance')
+    result = w()
+
+    assert result[1].value == expected
+    assert w.context.advance.call_count == len(expected)
