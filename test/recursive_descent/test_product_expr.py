f3ff8e4fa13c835e26807247d46310d6a086545e
Michael Caley <mjcaley@darkarctic.com>
2019-08-23 15:30:46 -0400

    Use mocks in parser tests

--- test/recursive_descent/test_product_expr.py
+++ test/recursive_descent/test_product_expr.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python3
 
+from functools import partial
+
 import pytest
 
-from pylang.lexer.lexer import Lexer
+from pylang.lexer.token import TokenType
 from pylang.recursive_descent import Parser
-from pylang.parse_tree import Integer, ProductExpression
+from pylang.parse_tree import Atom, ProductExpression
 
 
-@pytest.mark.parametrize('test_input,expected', [
-    ['42*42', ProductExpression],
-    ['42/42', ProductExpression],
-    ['42', Integer]
+@pytest.mark.parametrize('operator', [
+    TokenType.Multiply, TokenType.Divide
 ])
-def test_product_expr(test_input, expected):
-    l = Lexer.from_stream(test_input)
-    next(l)
-    p = Parser(lexer=l)
-
+def test_expression(tokens_from_types, operator, mocker):
+    p = Parser(lexer=tokens_from_types(TokenType.Integer, operator, TokenType.Integer))
+    mocked_expression = mocker.patch(
+        'pylang.recursive_descent.Parser.expression',
+        side_effect=(Atom(p.advance()) for _ in range(2))
+    )
     result = p.product_expr()
 
-    assert isinstance(result, expected)
+    assert isinstance(result, ProductExpression)
+    assert mocked_expression.call_count == 2
+    assert result.left == TokenType.Minus
+    assert result.operator.token_type == TokenType.Minus
+    assert isinstance(result.right, Atom)
+    assert result.expression == mocked_expression.return_value
+
+
+def test_returns_atom(tokens_from_types, mocker):
+    p = Parser(lexer=tokens_from_types(TokenType.True_))
+    mocked_atom = mocker.patch('pylang.recursive_descent.Parser.atom')
+    result = p.unary_expr()
+
+    assert mocked_atom.assert_called
+    assert result == mocked_atom.return_value
--- test/recursive_descent/test_unary_expr.py
+++ test/recursive_descent/test_unary_expr.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3
 
-import pytest
 
-from pylang.lexer.lexer import Lexer
+from pylang.lexer.token import TokenType
 from pylang.recursive_descent import Parser
-from pylang.parse_tree import Integer, UnaryExpression
+from pylang.parse_tree import UnaryExpression
 
 
-@pytest.mark.parametrize('test_input,expected', [
-    ['-42', UnaryExpression],
-    ['42', Integer]
-])
-def test_identifier(test_input, expected):
-    l = Lexer.from_stream(test_input)
-    next(l)
-    p = Parser(lexer=l)
-
+def test_negative_expression(tokens_from_types, mocker):
+    p = Parser(lexer=tokens_from_types(TokenType.Minus, TokenType.True_))
+    mocked_expression = mocker.patch('pylang.recursive_descent.Parser.expression')
     result = p.unary_expr()
 
-    assert isinstance(result, expected)
+    assert isinstance(result, UnaryExpression)
+    assert mocked_expression.assert_called
+    assert result.operator.token_type == TokenType.Minus
+    assert result.expression == mocked_expression.return_value
+
+
+def test_returns_atom(tokens_from_types, mocker):
+    p = Parser(lexer=tokens_from_types(TokenType.True_))
+    mocked_atom = mocker.patch('pylang.recursive_descent.Parser.atom')
+    result = p.unary_expr()
+
+    assert mocked_atom.assert_called
+    assert result == mocked_atom.return_value
