e00ebbb9e2a06c8cd0a698b489f0c83d625b63dd
Michael Caley <mjcaley@darkarctic.com>
2019-08-29 16:23:26 -0400

    Simplified atom tests

--- test/recursive_descent/test_atom.py
+++ test/recursive_descent/test_atom.py
@@ -1,58 +1,38 @@
 #!/usr/bin/env python3
 
-from functools import partial
-
 import pytest
 
 from pylang.lexer.token import TokenType
 from pylang.recursive_descent import Parser, UnexpectedTokenError
-from pylang.parse_tree import Atom, Boolean, Float, Identifier, Integer
 
 
-@pytest.mark.parametrize('token,parse_tree_cls', [
-    [TokenType.Integer, Integer],
-    [TokenType.Float, Float],
-    [TokenType.Identifier, Identifier],
-    [TokenType.True_, Boolean],
-    [TokenType.False_, Boolean]
+@pytest.mark.parametrize('token', [
+    TokenType.Integer,
+    TokenType.Float,
+    TokenType.Identifier,
+    TokenType.True_,
+    TokenType.False_
 ])
-def test_atom_types(token, parse_tree_cls, tokens_from_types):
-    p = Parser(lexer=tokens_from_types(token))
+def test_atom_types(token, tokens_from_types):
+    tokens = tokens_from_types(token)
+    p = Parser(lexer=tokens)
     result = p.atom()
 
-    assert isinstance(result, parse_tree_cls)
-    assert result.value.token_type == token
+    assert result.value is tokens[0]
 
 
 def test_expression(mocker, tokens_from_types):
-    p = Parser(lexer=tokens_from_types(TokenType.LParen, TokenType.Identifier, TokenType.RParen))
-    expression_return = mocker.stub()
-
-    def side_effect():
-        p.advance()
-        return expression_return
-
-    mocked_expression = mocker.patch(
-        'pylang.recursive_descent.Parser.expression',
-        side_effect=side_effect
-    )
+    tokens = tokens_from_types(TokenType.LParen, TokenType.Identifier, TokenType.RParen)
+    p = Parser(lexer=tokens)
+    expression_spy = mocker.spy(p, 'expression')
     result = p.atom()
 
-    assert result == expression_return
-    assert mocked_expression.call_count == 1
+    assert result.value is tokens[1]
+    assert expression_spy.assert_called
 
 
-def test_expression_raises_on_open_bracket(mocker, tokens_from_types):
+def test_expression_raises_on_open_bracket(tokens_from_types):
     p = Parser(lexer=tokens_from_types(TokenType.LParen, TokenType.Identifier, TokenType.Indent))
-
-    def side_effect():
-        p.advance()
-        return mocker.stub()
-
-    mocker.patch(
-        'pylang.recursive_descent.Parser.expression',
-        side_effect=side_effect
-    )
 
     with pytest.raises(UnexpectedTokenError):
         p.atom()
