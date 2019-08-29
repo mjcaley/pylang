c4edb312d65f738e12061f9b8722fa92eb1eab2d
Michael Caley <mjcaley@darkarctic.com>
2019-08-29 17:08:08 -0400

    Fixed block test

--- test/recursive_descent/test_block.py
+++ test/recursive_descent/test_block.py
@@ -1,20 +1,11 @@
 #!/usr/bin/env python3
 
-import pytest
-
-from pylang.lexer.lexer import Lexer
+from pylang.lexer.token import TokenType
 from pylang.recursive_descent import Parser
 
 
-@pytest.mark.parametrize('test_input', [
-    '\t42\n\t4.2\n\ttrue\n',
-    '    42\r\n    4.2\r\n    true\r\n'
-])
-def test_block(test_input):
-    l = Lexer.from_stream(test_input)
-    next(l)
-    p = Parser(lexer=l)
-
+def test_block(tokens_from_types):
+    p = Parser(lexer=tokens_from_types(TokenType.Indent, TokenType.True_, TokenType.Newline, TokenType.Dedent))
     result = p.block()
 
     assert isinstance(result, list)
