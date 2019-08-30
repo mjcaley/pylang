c5be44425c84567a7cdf6f6f3bb94c326c98274f
Michael Caley <mjcaley@darkarctic.com>
2019-08-30 11:56:18 -0400

    Fixed function_decl tests

--- test/recursive_descent/test_function_decl.py
+++ test/recursive_descent/test_function_decl.py
@@ -1,29 +1,94 @@
 #!/usr/bin/env python3
 
-import pytest
-
-from pylang.lexer.lexer import Lexer
-from pylang.recursive_descent import Parser, UnexpectedTokenError
+from pylang.lexer.token import TokenType
+from pylang.recursive_descent import Parser
 from pylang.parse_tree import FunctionDecl
 
 
-@pytest.mark.parametrize('test_input,name,num_params,return_type', [
-    ['func functionName()', 'functionName', 0, None],
-    ['func functionName(abc)', 'functionName', 1, None],
-    ['func functionName(abc, def, ghi)', 'functionName', 3, None],
-    ['func functionName() : int', 'functionName', 0, 'int'],
-    ['func functionName(abc) : int', 'functionName', 1, 'int'],
-    ['func functionName(abc, def, ghi) : int', 'functionName', 3, 'int'],
-])
-def test_parameters(test_input, name, num_params, return_type):
-    l = Lexer.from_stream(test_input)
-    next(l)
-    p = Parser(lexer=l)
-
+def test_function_no_param_no_return(tokens_from_types):
+    tokens = tokens_from_types(
+        TokenType.Function,
+        TokenType.Identifier,
+        TokenType.LParen,
+        TokenType.RParen,
+        TokenType.Assignment,
+        TokenType.Newline
+    )
+    tokens[1].value = 'name'
+    p = Parser(lexer=tokens)
     result = p.function_decl()
 
     assert isinstance(result, FunctionDecl)
-    assert name == result.name.value.value
-    assert num_params == len(result.parameters)
-    if return_type:
-        assert result.return_type.value.value == return_type
+    assert result.name.value.value == 'name'
+    assert result.parameters == []
+    assert result.return_type is None
+
+
+def test_function_one_param_no_return(tokens_from_types):
+    tokens = tokens_from_types(
+        TokenType.Function,
+        TokenType.Identifier,
+        TokenType.LParen,
+        TokenType.Identifier,
+        TokenType.RParen,
+        TokenType.Assignment,
+        TokenType.Newline
+    )
+    tokens[1].value = 'name'
+    tokens[3].value = 'param'
+    p = Parser(lexer=tokens)
+    result = p.function_decl()
+
+    assert isinstance(result, FunctionDecl)
+    assert result.name.value.value == 'name'
+    assert result.parameters[0].value.value == 'param'
+    assert len(result.parameters) == 1
+    assert result.return_type is None
+
+
+def test_function_multiple_param_no_return(tokens_from_types):
+    tokens = tokens_from_types(
+        TokenType.Function,
+        TokenType.Identifier,
+        TokenType.LParen,
+        TokenType.Identifier,
+        TokenType.Comma,
+        TokenType.Identifier,
+        TokenType.RParen,
+        TokenType.Assignment,
+        TokenType.Newline
+    )
+    tokens[1].value = 'name'
+    tokens[3].value = 'param1'
+    tokens[5].value = 'param2'
+    p = Parser(lexer=tokens)
+    result = p.function_decl()
+
+    assert isinstance(result, FunctionDecl)
+    assert result.name.value.value == 'name'
+    assert result.parameters[0].value.value == 'param1'
+    assert result.parameters[1].value.value == 'param2'
+    assert len(result.parameters) == 2
+    assert result.return_type is None
+
+
+def test_function_no_param_with_return(tokens_from_types):
+    tokens = tokens_from_types(
+        TokenType.Function,
+        TokenType.Identifier,
+        TokenType.LParen,
+        TokenType.RParen,
+        TokenType.Colon,
+        TokenType.Identifier,
+        TokenType.Assignment,
+        TokenType.Newline
+    )
+    tokens[1].value = 'name'
+    tokens[5].value = 'return'
+    p = Parser(lexer=tokens)
+    result = p.function_decl()
+
+    assert isinstance(result, FunctionDecl)
+    assert result.name.value.value == 'name'
+    assert len(result.parameters) == 0
+    assert result.return_type.value.value == 'return'
